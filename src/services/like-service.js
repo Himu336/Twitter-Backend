c17157b2316de2f24c541f2d83a587fec4614773
Himansh <himanshbansal200521@gmail.com>
2025-09-02 16:57:34 +0530

    ADD: added polymorphic property in toggle like logic to switch btw tweet and comment

--- src/services/like-service.js
+++ src/services/like-service.js
@@ -2,36 +2,51 @@ import { StatusCodes } from "http-status-codes";
 import AppError from "../utils/errors/app-error.js";
 
 import LikeRepository from "../repositories/like-repository.js";
+
 import TweetRepository from "../repositories/tweet-repository.js";
+import CommentRepository from "../repositories/comment-repository.js";
 
 const likeRepository = new LikeRepository();
 const tweetRepository = new TweetRepository();
+const commentRepository = new CommentRepository();
+
 
 async function toggleLike(modelId, modelType, userId) {
     try {
-        const model = await tweetRepository.get(modelId);
-        if (!model) {
-            throw new AppError('The tweet you requested is not present', StatusCodes.NOT_FOUND);
+        let repository;
+        if (modelType === "Tweet") {
+            repository = tweetRepository;
+        } else if (modelType === "Comment") {
+            repository = commentRepository;
+        } else {
+            throw new AppError('Invalid model type for like', StatusCodes.BAD_REQUEST);
         }
+
+        const model = await repository.get(modelId);
+        if (!model) {
+            throw new AppError(`The ${modelType.toLowerCase()} you requested is not present`, StatusCodes.NOT_FOUND);
+        }
+
         const existingLike = await likeRepository.findBy({
             user: userId,
             onModel: modelType,
             likeable: modelId
         });
+
         if (existingLike) {
             await likeRepository.destroy(existingLike.id);
-            model.likes--;
-            await tweetRepository.update(modelId, model);
+            model.likes = (model.likes > 0 ? model.likes - 1 : 0);
+            await repository.update(modelId, model);
             return false;
         }
-        const like = await likeRepository.create({
+
+        await likeRepository.create({
             user: userId,
             onModel: modelType,
             likeable: modelId
         });
-        model.likes++;
-
-        await tweetRepository.update(modelId, model);
+        model.likes = (model.likes || 0) + 1;
+        await repository.update(modelId, model);
         return true;
     } catch (error) {
         if (error.statusCode == StatusCodes.NOT_FOUND) {
