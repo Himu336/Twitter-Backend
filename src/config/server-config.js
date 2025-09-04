dc9bca5e213074b086eaa1d24d327a922258926d
Himansh <himanshbansal200521@gmail.com>
2025-09-04 16:42:11 +0530

    ADD: added image upload feature using aws-sdk and multer

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,3 +6,6 @@ export const PORT = process.env.PORT;
 export const ATLAS_DB_URL = process.env.ATLAS_DB_URL;
 export const NODE_ENV = process.env.NODE_ENV;
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+export const REGION = process.env.REGION;
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+export const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID;
--- src/controllers/tweet-controller.js
+++ src/controllers/tweet-controller.js
@@ -1,19 +1,26 @@
 import { TweetService } from '../services/tweet-service.js';
 import { StatusCodes } from "http-status-codes";
+import upload from "../config/file-upload-s3.js";
 
 import SuccessResponse from '../utils/common/success-response.js';
 import ErrorResponse from '../utils/common/error-response.js';
 
 async function createTweet(req, res) {
+    const singleUploader = upload.single('image');
     try {
-        const tweet = await TweetService.create({
-            content: req.body.content,
-            author: req.body.author
+        singleUploader(req, res, async function(err) {
+            if(err) {
+                console.log(err);
+            }
+            console.log(req.file);
+            const payload = {...req.body};
+            payload.image = req.file.location;
+            const tweet = await TweetService.create(payload);
+            SuccessResponse.data = tweet;
+            return res
+                .status(StatusCodes.CREATED)
+                .json(SuccessResponse);
         });
-        SuccessResponse.data = tweet;
-        return res
-            .status(StatusCodes.CREATED)
-            .json(SuccessResponse);
     } catch(error) {
         ErrorResponse.error = error;
         return res
